97d22feef48c44790a8381429338a4b3dc2bc2a8
Daniel Abel <daniel@zkreations.com>
2025-06-03 15:21:14 -0500

    refactor: remove i18nText module and integrate i18n directly in copyClipboard functionality

--- assets/js/components/copyClipboard.js
+++ assets/js/components/copyClipboard.js
@@ -1,6 +1,9 @@
-import { i18nText } from './i18nText.js'
-
 const blocks = document.querySelectorAll('pre > code')
+const article = document.querySelector('article')
+
+const i18n = JSON.parse(article.getAttribute('data-i18n'))
+const COPY_TEXT = i18n.copy
+const COPIED_TEXT = i18n.copied
 
 function addCopyButtons () {
   if (!navigator || !navigator.clipboard || blocks.length === 0) return
@@ -12,14 +15,14 @@ function addCopyButtons () {
     button.className = 'code-copy'
 
     button.setAttribute('data-tts', 'up-right')
-    button.setAttribute('aria-label', i18nText('copy'))
+    button.setAttribute('aria-label', COPY_TEXT)
     button.innerHTML = '<svg class="i i-copy" viewBox="0 0 24 24"><path d="M5 15H4a2 2 0 0 1-2-2V4a2 2 0 0 1 2-2h9a2 2 0 0 1 2 2v1"></path><rect width="13" height="13" x="9" y="9" rx="2"></rect></svg>'
     button.addEventListener('click', function () {
       clipboard.writeText(codeBlock.textContent).then(function () {
         button.blur()
-        button.setAttribute('aria-label', i18nText('copied'))
+        button.setAttribute('aria-label', COPIED_TEXT)
         setTimeout(function () {
-          button.setAttribute('aria-label', i18nText('copy'))
+          button.setAttribute('aria-label', COPY_TEXT)
         }, 2000)
       })
     })
